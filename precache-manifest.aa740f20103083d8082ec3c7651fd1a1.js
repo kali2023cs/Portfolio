0c77bc918926565f473834bd89dd4f284d2896fb
unknown <kalidassr280gmail.com>
2023-10-10 12:49:05 +0530

    Updates

---
+++ precache-manifest.aa740f20103083d8082ec3c7651fd1a1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322d925d45961b3e612026f62ca14522",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1a5db1d5992c911f653b",
+    "url": "/Portfolio/static/css/main.f9dac71b.chunk.css"
+  },
+  {
+    "revision": "6b3bdfc335fe7981a050",
+    "url": "/Portfolio/static/js/2.d4a9c164.chunk.js"
+  },
+  {
+    "revision": "1a5db1d5992c911f653b",
+    "url": "/Portfolio/static/js/main.82edc153.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "496ea08f74efaa3857600f8f70bd4bf3",
+    "url": "/Portfolio/static/media/adobe.496ea08f.png"
+  },
+  {
+    "revision": "b3f8130557a6a5b24e4d8a9e40047414",
+    "url": "/Portfolio/static/media/alight_icon.b3f81305.png"
+  },
+  {
+    "revision": "bae53fc49ccdc4f4b25e522d0c0cb5ef",
+    "url": "/Portfolio/static/media/app-design.bae53fc4.png"
+  },
+  {
+    "revision": "3560f7ac96db464e036dd60b9fc9c917",
+    "url": "/Portfolio/static/media/facebook.3560f7ac.png"
+  },
+  {
+    "revision": "3530119e779bc59d7df0860bbd24c9a2",
+    "url": "/Portfolio/static/media/image.3530119e.png"
+  },
+  {
+    "revision": "51685f17e05ba2b76687461e705e9a59",
+    "url": "/Portfolio/static/media/instagram.51685f17.png"
+  },
+  {
+    "revision": "cd83fd324674d5f894557d38dd3ce0b2",
+    "url": "/Portfolio/static/media/microsoft.cd83fd32.png"
+  },
+  {
+    "revision": "15b0f259de9883ae5aa7cdad9ce0e1c0",
+    "url": "/Portfolio/static/media/portfolio-1.15b0f259.png"
+  },
+  {
+    "revision": "d81c32d1b6442b7b2a9c49e3ba8aaa47",
+    "url": "/Portfolio/static/media/portfolio-2.d81c32d1.png"
+  },
+  {
+    "revision": "a45f2f711c1283bc521c7096778b7a6a",
+    "url": "/Portfolio/static/media/portfolio-3.a45f2f71.png"
+  },
+  {
+    "revision": "808ace36c8c1adf2a57aa8ec1891d350",
+    "url": "/Portfolio/static/media/portfolio-4.808ace36.png"
+  },
+  {
+    "revision": "60e9c57b33b306d55383f63f917242a1",
+    "url": "/Portfolio/static/media/portfolio-5.60e9c57b.png"
+  },
+  {
+    "revision": "48b0cf2c9dcd3262d7c474527c7276db",
+    "url": "/Portfolio/static/media/portfolio-6.48b0cf2c.png"
+  },
+  {
+    "revision": "1e1ba20cfa4a4c86d66437632e35df2f",
+    "url": "/Portfolio/static/media/ui-design.1e1ba20c.png"
+  },
+  {
+    "revision": "52d45461c01ab0f6f898cb26726e1550",
+    "url": "/Portfolio/static/media/walmart.52d45461.png"
+  }
+]);
